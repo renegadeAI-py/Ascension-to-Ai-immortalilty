6f8766f54e053260512aa0fc89122ef8913c857f
renegadeAI-py <supremebiof4@gmail.com>
2026-05-21 13:20:45 +0200

    create a BTTH novel.py

---
+++ mad lab.py
@@ -0,0 +1,14 @@
+#renagadeAi 
+#age 16
+#day:1
+#project 2
+#This a mini Q and Story 🎯 about BTTH
+
+name=input("gimme a Donghua MC:")
+enemy=input("gimme his blood enemy:")
+donghua=input("gimme the Donghua's name:")
+continent_name=input("gimme a continent name:")
+title=input("gimme a title:")
+for I in range(10)
+print(name+"still cultivating....")
+print(" when "+ name +" ascended to the realm of Dou Di in the "+ continent_name+"he defeated"+ enemy + "brutaly and earned the novel"+ Donghua +"the Tittle"+ title)
