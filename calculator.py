96d46689664de3adaa4300af1118f89b71a06f3a
renegadeAI-py <supremebiof4@gmail.com>
2026-05-21 12:47:10 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1= input("choose your number")
+num2=input("choose number")
+choice=input("add,multipy,divide,subtract:")
+num1=float(num1)
+num2=float(num2)
+if choice=="add":
+    answer=num1 + num2
+elif choice=="multiply":
+    answer= num1 * num2
+elif choice =="divide":
+    answer=num1 / num2
+elif choice=="subtract":
+    answer= num1 - num2
+else:
+    answer ="invalid"
+print("answer is "+str(answer))
