d53d1fa6681552ec915e136ea9cdc6bc21098eda
renegadeAI-py <supremebiof4@gmail.com>
2026-05-23 20:25:48 +0200

    Create renegade immortal: explained.py
    #renegade story using:
    file operations
    Dictionaries
    #project_status:finished
    #bugs:🤔<=0

---
+++ renegade immortal: explained.py
@@ -0,0 +1,35 @@
+realm="ascentant"
+with open("renagade_immortal.txt","w") as file:
+    file.write("wang lin broke through\n")
+    file.write("Violent purple Aura flooded the area\n")
+    file.write("Absolute Dominance====\n")
+    file.write("wang lin has reached "+ realm)
+    
+with open("renagade_immortal.txt","r") as file:
+    text=file.read()
+    print(text)
+with open("renagade_immortal.txt","w") as file:
+    file.write("Wang lin is now the ZUZAKU\n")
+    file.write("Everyone now submits\n")
+    file.write("Wang lin now represents the peak of existance\n")
+    file.write("He is now the Ruthless:Sovereign")
+with open("renagade_immortal.txt","a") as file:
+   file.write("\nWang lin has become a god")
+cultivator={"name":{"realm": "ascendant",
+                                      "attack=": 1000000,
+                                      "Health=": "undying",
+                                      "wife=": "Li_muwan",
+                                      "Status=":"ZUZAKU"},
+                      "origin":{"planet=":"Vermillion_Bird_planet",
+                                                           "province":"zhou_country"}}
+with open("renagade_immortal.txt","a") as file:
+      file.write("\n"+ str(cultivator["name"]))
+      file.write("\n" + str(cultivator["origin"]))
+with open("renagade_immortal.txt","r") as file:
+                                     
+                                     text3=file.read()
+                                     print(text3)
+                                     #finished project
+                                     #difficuly: super easy
+                                     #progress:9/10 lessons masterd
+                                     #duration:2,5 days
